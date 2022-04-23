8a4025e897624ec02536de233d5259b15f2d54c7
Elizabeth <ekkjohnson@gmail.com>
2022-04-23 11:03:07 -0500

    employee chart

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,11 @@ CREATE TABLE role(
     dept_id INT NOT NULL,
     FOREIGN KEY (dept_id) REFERENCES department(id)
 );
+
+CREATE Table employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VAHCHAR(30),
+  last_name VAHCHAR(30),
+  node_id INT NOT NULL,
+  manager_id INT NOT NULL
+)
