bccfee9d738699496d44cc49a0b217135a875dd8
Elizabeth <ekkjohnson@gmail.com>
2022-04-24 13:29:02 -0500

    final queries

--- queryFunction.js
+++ queryFunction.js
@@ -41,11 +41,86 @@ function addDept () {
         startApp();
     })
 }
+function addRole () {
+    runPrompt = require('./db/index')
+    db.query('SELECT * FROM emp_department', (err, res)=>{
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'deptId',
+                message: 'What department is your employee in?',
+                choices: res.map(department=>department.name)
+            },
+            {
+                type: 'input',
+                name: 'addSalary',
+                message: "What is your employees salary?",
+            },
+            {
+                type: 'input',
+                name: "addTitle",
+                message: "What is your employees job title?"
+            }
+        ]).then(response=>{
+            const selectedDept = res.find(department=>department.name === response.deptId)
+            db.query('INSERT INTO emp_role SET ?', {
+                salary: response.addSalary,
+                title: response.addTitle,
+                department_id: selectedDept.id
+            })
+            console.log ('Added new role')
+            startApp();
+        })
+    })
+}
 
+function addEmp () {
+    runPrompt = require('./db/index')
+    db.query('SELECT * FROM emp_role', (err, res)=>{
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'empId',
+                message: 'What role is the role of your new employee?',
+                choices: res.map(role=>role.title)
+            },
+            {
+                type: 'input',
+                name: 'firstName',
+                message: "What is your employees first name?",
+            },
+            {
+                type: 'input',
+                name: "lastName",
+                message: "What is your employees last name?"
+            },
+            {
+                type: 'choices',
+                name: "empManager",
+                message: "Who is your employees manager?",
+                choices: res.map(role=>role.manager_id)
+            },
 
+        ]).then(response=>{
+            const selectedTitle = res.find(role=>role.title === response.empId)
+            const selectedManager = res.find(role=>role.manager_id === response.empManager)
+            db.query('INSERT INTO emp_role SET ?', {
+                first_name: response.firstName,
+                last_name: response.last_name,
+                role_id: selectedTitle.id,
+                manager_id: selectedManager.id
+            })
+            console.log ('Added new employee')
+            startApp();
+        })
+    })
+}
 
+function updateEmp (){
+    db.query ('Select = ? FROM employee')
+}
 
 module.exports = {
-    allDepts
+    allDepts, allRoles, allEmps, addDept, addRole, addEmp, updateEmp
     // allDepts : allDepts
 }
