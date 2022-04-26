f76c269c0b63c40ee1be8845ff73e987d45e2218
Elizabeth <ekkjohnson@gmail.com>
2022-04-25 20:42:57 -0500

    update employee

--- queryFunction.js
+++ queryFunction.js
@@ -1,55 +1,55 @@
 const db = require('./connection')
-const inquirer=require('inquirer')
+const inquirer = require('inquirer')
 
 let startApp;
 function allDepts() {
-    startApp=require('./db/index')
+    startApp = require('./db/index')
     db.query('SELECT * FROM emp_dept', function (err, results) {
         console.log(results);
         startApp();
     });
 }
-function allRoles(){
-    startApp=require('./db/index')
+function allRoles() {
+    startApp = require('./db/index')
     db.query('SELECT * FROM emp_role', function (err, results) {
         console.log(results);
         startApp();
     });
 }
 
-function allEmps(){
-    startApp=require('./db/index')
+function allEmps() {
+    startApp = require('./db/index')
     db.query('SELECT * FROM employee', function (err, results) {
         console.log(results);
         startApp();
     });
 }
 
-function addDept () {
-    runPrompt = require('./db/index')
-    inquirer.prompt ([
+function addDept() {
+    startApp = require('./db/index')
+    inquirer.prompt([
         {
             type: 'input',
             name: 'addDept',
             message: "What is the name of the department you would like to add?"
 
         }
-    ]).then(response =>{
+    ]).then(response => {
         db.query('INSERT INTO emp_department SET ?', {
             name: response.addDept
         })
         startApp();
     })
 }
-function addRole () {
-    runPrompt = require('./db/index')
-    db.query('SELECT * FROM emp_department', (err, res)=>{
+function addRole() {
+    startApp = require('./db/index')
+    db.query('SELECT * FROM emp_department', (err, res) => {
         inquirer.prompt([
             {
                 type: 'list',
                 name: 'deptId',
                 message: 'What department is your employee in?',
-                choices: res.map(department=>department.name)
+                choices: res.map(department => department.name)
             },
             {
                 type: 'input',
@@ -61,28 +61,28 @@ function addRole () {
                 name: "addTitle",
                 message: "What is your employees job title?"
             }
-        ]).then(response=>{
-            const selectedDept = res.find(department=>department.name === response.deptId)
+        ]).then(response => {
+            const selectedDept = res.find(department => department.name === response.deptId)
             db.query('INSERT INTO emp_role SET ?', {
                 salary: response.addSalary,
                 title: response.addTitle,
                 department_id: selectedDept.id
             })
-            console.log ('Added new role')
+            console.log('Added new role')
             startApp();
         })
     })
 }
 
-function addEmp () {
-    runPrompt = require('./db/index')
-    db.query('SELECT * FROM emp_role', (err, res)=>{
+function addEmp() {
+    startApp = require('./db/index')
+    db.query('SELECT * FROM emp_role', (err, res) => {
         inquirer.prompt([
             {
                 type: 'list',
                 name: 'empId',
                 message: 'What role is the role of your new employee?',
-                choices: res.map(role=>role.title)
+                choices: res.map(role => role.title)
             },
             {
                 type: 'input',
@@ -98,29 +98,39 @@ function addEmp () {
                 type: 'choices',
                 name: "empManager",
                 message: "Who is your employees manager?",
-                choices: res.map(role=>role.manager_id)
+                choices: res.map(role => role.manager_id)
             },
 
-        ]).then(response=>{
-            const selectedTitle = res.find(role=>role.title === response.empId)
-            const selectedManager = res.find(role=>role.manager_id === response.empManager)
+        ]).then(response => {
+            const selectedTitle = res.find(role => role.title === response.empId)
+            const selectedManager = res.find(role => role.manager_id === response.empManager)
             db.query('INSERT INTO emp_role SET ?', {
                 first_name: response.firstName,
-                last_name: response.last_name,
+                last_name: response.lastName,
                 role_id: selectedTitle.id,
                 manager_id: selectedManager.id
             })
-            console.log ('Added new employee')
+            console.log('Added new employee')
             startApp();
         })
     })
 }
 
-function updateEmp (){
-    db.query ('Select = ? FROM employee')
+function updateEmp() {
+    startApp = require('./db/index')
+    db.query('Select * FROM employee', (err, empResult) => {
+        if (err) throw err;
+        const updateEmp = []
+        empResult.forEach(({ first_name, last_name, id }) => {
+            updateEmpInfo.push({
+                name: first_name + " " + last_name,
+                value: id
+            })
+        })
+    })
 }
 
-function finish (){
+function finish() {
     console.log("Completed")
     process.exit()
 }
