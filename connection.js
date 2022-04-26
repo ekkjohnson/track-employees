dc9ad3a9ed1c9bfcb920ab2b28a82a32aceb78b0
Elizabeth <ekkjohnson@gmail.com>
2022-04-25 20:03:21 -0500

    connection to env

--- connection.js
+++ connection.js
@@ -4,8 +4,8 @@ const inquirer = require ('inquirer');
 const db=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 },
 console.log('connected to the database')
 );
